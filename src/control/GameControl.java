315f375516f9c14e223dfe662d9c88b62b998722
Ceo <Ceo@10.20.169.163>
2016-05-30 12:42:06 +0200

    Merge origin/master
    Conflicts:
    	src/view/Main.java

--- src/control/GameControl.java
+++ src/control/GameControl.java
@@ -9,29 +9,38 @@ import model.Room;
  */
 public class GameControl {
 
-    public GameControl(String name) {
-        createPlayer(name);
+      Room currentRoom;
+    
+    public GameControl() {
+    createRooms();
     }
 
-    
+   
     
     private void createRooms() {
 
+       
+        
         Room room1 = new Room("You are standing in the Entrance ", 0);
         Room room2 = new Room("Tomb", 0);
         Room room3 = new Room("Sacrefice Pit", 0);
         Room room4 = new Room("S/M Dungeon ", 0);
         Room room5 = new Room("McDonalds", 0);
 
+         //Room 1
         room1.east = room2;
         room1.north = room3;
         room1.west = room4;
         room1.south = room5;
-        //Room 1
+        
+        room2.west = room1;
+       
 
+        
+        currentRoom = room1;
     }
 
-    private void createPlayer(String name) {
+    public void createPlayer(String name) {
         Player player = new Player();
 
         player.setName(name);
@@ -39,5 +48,56 @@ public class GameControl {
         System.out.println(player.getName());
 
     }
+    
+    public void move(String direction){
+        
+        
+        
+        switch (direction) {
+            case "north":
+                
+                currentRoom = currentRoom.north;
+                
+                
+                break;
+                
+            case "west":
+                
+                currentRoom = currentRoom.west;
+                break;
+                
+            case "east":
+                
+                currentRoom = currentRoom.east;
+                break;
+                
+            case "south":
+                
+                currentRoom = currentRoom.south;
+                break;
+                
+                
+                
+            default:
+                System.out.println("Error in movement");
+                throw new AssertionError();
+        }
+            
+        
+
+        
+    }
+
+    public Room getCurrentRoom() {
+        return currentRoom;
+    }
+
+    public void setCurrentRoom(Room currentRoom) {
+        this.currentRoom = currentRoom;
+    }
+
+  
+    
+    
 
 }
--- src/view/Game.java
+++ src/view/Game.java
@@ -12,23 +12,50 @@ import java.util.Scanner;
  *
  * @author Ceo
  */
-
-
 public class Game {
 
+    GameControl gc = new GameControl();
+
     private Scanner sca = new Scanner(System.in);
-    
+
     public Game() {
         NewGame();
     }
-    
-    
-    private void NewGame(){
+
+    private void NewGame() {
+
+        boolean gameActive = true;
+
         System.out.println("What's your name?");
         String name = sca.next();
-        GameControl gc = new GameControl(name);
-        
+        gc.createPlayer(name);
+        System.out.println("");
+
+        while (gameActive) {
+            System.out.println("Where would you like to move to?");
+            String move = sca.next();
+            movement(move);
+        }
     }
-    
-    
+
+    private void movement(String move) {
+
+        System.out.println(move);
+        if (move.equalsIgnoreCase("North") || move.equalsIgnoreCase("n")) {
+            gc.move(move);
+        } else if (move.equalsIgnoreCase("South") || move.equalsIgnoreCase("s")) {
+            gc.move(move);
+        } else if (move.equalsIgnoreCase("East") || move.equalsIgnoreCase("e")) {
+            gc.move(move);
+        } else if (move.equalsIgnoreCase("West") || move.equalsIgnoreCase("w")) {
+            gc.move(move);
+
+        } else {
+            System.out.println("Please enter a direction");
+        }
+
+        System.out.println(gc.getCurrentRoom().getDescription());
+
+    }
+
 }
